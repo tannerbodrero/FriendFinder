a06231fbb132df966995512091bd81bbebcd5c52
Tanner Bodrero <tanner.bodrero@gmail.com>
2019-09-07 20:03:05 -0700

    creating home page

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ var app = express();
 
 var PORT = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + "/app/css"));
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
