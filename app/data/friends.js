6a9837e40984d16102c99c86bc77f9edcb9475cb
Tanner Bodrero <tanner.bodrero@gmail.com>
2019-09-21 17:20:56 -0700

    Finished adding functionality to survey

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Spongebob",
+		photo: "https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/SpongeBob_SquarePants_character.svg/1200px-SpongeBob_SquarePants_character.svg.png",
+		scores: [5, 3, 2, 4, 1, 1, 5, 2, 4, 3]
+	},
+	{
+		name: "Sandy",
+		photo: "https://upload.wikimedia.org/wikipedia/en/thumb/a/a0/Sandy_Cheeks.svg/1200px-Sandy_Cheeks.svg.png",
+		scores: [2, 5, 3, 1, 4, 5, 1, 4, 3, 2]
+	},
+	{
+		name: "Mr. Krabs",
+		photo: "https://upload.wikimedia.org/wikipedia/en/thumb/f/f8/Mr._Krabs.svg/1200px-Mr._Krabs.svg.png",
+		scores: [1, 4, 5, 2, 3, 4, 2, 3, 1, 5]
+	},
+	{
+		name: "Patrick",
+		photo: "https://upload.wikimedia.org/wikipedia/en/thumb/3/33/Patrick_Star.svg/1200px-Patrick_Star.svg.png",
+		scores: [4, 2, 1, 5, 2, 3, 3, 1, 5, 4]
+	},
+	{
+		name: "Squidward",
+		photo: "https://upload.wikimedia.org/wikipedia/en/thumb/8/8f/Squidward_Tentacles.svg/1200px-Squidward_Tentacles.svg.png",
+		scores: [3, 1, 4, 3, 5, 2, 4, 5, 2, 1]
+	}
+];
+
+module.exports = friends;
